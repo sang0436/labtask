361d7f2f506081927092f057ad98cf9dea196228
sang0436 <63089782+sang0436@users.noreply.github.com>
2021-09-08 18:07:52 +0900

    second commit

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 import tensorflow as tf
-
 from tensorflow.keras import datasets, layers, models
-# 모델을 만들 때 keras sequential API 사용
 import matplotlib.pyplot as plt
-# 이미지를 표현하기 위해 점들을 찍을 것
+import numpy as np
 
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 # CIFAR-10 dataset 다운로드
 
-train_images, test_images = train_images / 255.0, test_images / 255.0
-# RGB 픽셀값을 0~1 사이 정규화
-
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
                'dog', 'frog', 'horse', 'ship', 'truck']
 
-plt.figure(figsize=(10, 10))
-for i in range(10):
-    plt.subplot(5, 5, i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i][0]])
-plt.show()
+# 데이터 정보
+print("Training data")
+print("Number of examples : ", train_images.shape[0])
+print("Number of channels : ", train_images.shape[3])
+print("Image size : ", train_images.shape[1], train_images.shape[2])
+print("-----")
+print("Test data")
+print("Number of examples : ", test_images.shape[0])
+print("Number of channels : ", test_images.shape[3])
+print("Image size : ", test_images.shape[1], test_images.shape[2])
+print("-----")
+
+print("mean before normalization : ", np.mean(train_images))
+print("std before normalization : ", np.std(train_images))
+
+mean = [0, 0, 0]
+std = [0, 0, 0]
+new_train_images = np.ones(train_images.shape)
+new_test_images = np.ones(test_images.shape)
+
+for i in range(3):  # 채널이 3개이므로 각 채널마다의 평균, 표준편차 (train set 만)
+    mean[i] = np.mean(train_images[:, :, :, i])
+    std[i] = np.std(train_images[:, :, :, i])
+
+for i in range(3):  # 정규화 작업 (train set, test set 모두)
+    new_train_images[:, :, :, i] = train_images[:, :, :, i] - mean[i]
+    new_train_images[:, :, :, i] = new_train_images[:, :, :, i] / std[i]
+    new_test_images[:, :, :, i] = test_images[:, :, :, i] - mean[i]
+    new_test_images[:, :, :, i] = new_test_images[:, :, :, i] / std[i]
+
+train_images = new_train_images
+test_images = new_test_images
+
+print("-----")
+print("mean after normalization : ", np.mean(train_images))
+print("std after normalization : ", np.std(train_images))
+
+
+
 
